ad84efcf8b2a79458c832096cd29eca46b2b7414
Darshan <itsponikar@gmail.com>
2023-01-16 17:04:07 +0530

    fix: tsconfig file updated, remove ts entry file

--- expo/plugin/bootsplash-plugin.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigPlugin } from "expo/config-plugins";
-import { withAndroidPlugin } from "./android";
-
-export const withBootsplash: ConfigPlugin = (config) => {
-  return withAndroidPlugin(config);
-};
