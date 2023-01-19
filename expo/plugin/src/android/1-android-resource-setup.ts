90c8ae8f23625154b1606a45add3f17c2f3044d2
Darshan <itsponikar@gmail.com>
2023-01-19 14:42:36 +0530

    fix: resource xml file issue

--- expo/plugin/src/android/1-android-resource-setup.ts
+++ expo/plugin/src/android/1-android-resource-setup.ts
@@ -1,16 +1,32 @@
 import { withAndroidStyles } from "@expo/config-plugins";
-
 export const setupAndroidResource = (config) => {
-  // add styles.xml
-  const styles = `
-    <style name="BootTheme" parent="Theme.SplashScreen">
-    <item name="windowSplashScreenBackground">@color/bootsplash_background</item>
-    <item name="windowSplashScreenAnimatedIcon">@mipmap/bootsplash_logo</item>
-    <item name="postSplashScreenTheme">@style/AppTheme</item>
-  </style>` as any;
-
   const updatedConfig = withAndroidStyles(config, (updatedConfig) => {
-    updatedConfig.modResults.resources.style?.push(styles);
+    updatedConfig.modResults.resources.style?.push({
+      $: {
+        name: "BootTheme",
+        parent: "Theme.SplashScreen",
+      },
+      item: [
+        {
+          $: {
+            name: "windowSplashScreenBackground",
+          },
+          _: "@color/bootsplash_background",
+        },
+        {
+          $: {
+            name: "windowSplashScreenAnimatedIcon",
+          },
+          _: "@mipmap/bootsplash_logo",
+        },
+        {
+          $: {
+            name: "postSplashScreenTheme",
+          },
+          _: "@style/AppTheme",
+        },
+      ],
+    });
     return updatedConfig;
   });
 
