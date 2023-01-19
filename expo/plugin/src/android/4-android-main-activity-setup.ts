524e16584420326ec5b61b9da9ec79db3922c348
Darshan <itsponikar@gmail.com>
2023-01-19 14:44:07 +0530

    fix: android main activity config file

--- expo/plugin/src/android/4-android-main-activity-setup.ts
+++ expo/plugin/src/android/4-android-main-activity-setup.ts
@@ -2,20 +2,39 @@ import { withMainActivity } from "expo/config-plugins";
 
 export const setupAndroidMainActivity = (config) => {
   return withMainActivity(config, (updatedConfig) => {
-    // add import statement
+    // add import statement at the top new line after package statement
     updatedConfig.modResults.contents =
       updatedConfig.modResults.contents.replace(
-        /import\sandroid\.os\.Bundle;/,
-        `import android.os.Bundle;
-        import com.zoontek.rnbootsplash.RNBootSplash;`,
+        /package\s(.+?);/,
+        `package $1;
+         import com.zoontek.rnbootsplash.RNBootSplash;
+        `,
       );
 
-    updatedConfig.modResults.contents =
-      updatedConfig.modResults.contents.replace(
-        /super\.onCreate/,
-        `super.onCreate(null);
-        RNBootSplash.init(this);`,
+    // find onCreate method and add RNBootSplash.init at the end of the method
+
+    const onCreateMethod = updatedConfig.modResults.contents.match(
+      /protected void onCreate\(Bundle savedInstanceState\)\s\{([\s\S]*?)\}/, // match onCreate method
+    )?.[0];
+
+    if (onCreateMethod) {
+      // remove setTheme(R.style.AppTheme); from onCreate method
+      const onCreateMethodWithoutSetTheme = onCreateMethod.replace(
+        /setTheme\(R\.style\.AppTheme\);/g,
+        "",
       );
+
+      // add RNBootSplash.init at the end of onCreate method
+      const onCreateMethodWithBootSplashInit =
+        onCreateMethodWithoutSetTheme.replace(/}/, `RNBootSplash.init(this);}`);
+
+      // replace onCreate method with new onCreate method
+      updatedConfig.modResults.contents =
+        updatedConfig.modResults.contents.replace(
+          onCreateMethod,
+          onCreateMethodWithBootSplashInit,
+        );
+    }
 
     return updatedConfig;
   });
