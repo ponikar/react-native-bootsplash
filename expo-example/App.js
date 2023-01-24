cb5cc8a162b02aec2736703192cdc90d430e5fa1
Darshan <itsponikar@gmail.com>
2023-01-24 12:02:04 +0530

    feat: added js logic to run bootsplash

--- expo-example/App.js
+++ expo-example/App.js
@@ -1,7 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import RNBootSplash from "react-native-bootsplash";
 
 export default function App() {
+  useEffect(() => {
+    RNBootSplash.hide({ duration: 250 });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
